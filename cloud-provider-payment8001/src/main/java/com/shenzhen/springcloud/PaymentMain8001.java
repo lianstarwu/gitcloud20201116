0a027f97077f8beb5cdd25323a2e48eeea19ed1a
starwu <starwu@atguigu.com>
2020-11-16 15:55:12 +0800

    springcloud test

---
+++ cloud-provider-payment8001/src/main/java/com/shenzhen/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.shenzhen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: wuxingxing
+ * @Date: 2020/11/16
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class, args);
+    }
+}
